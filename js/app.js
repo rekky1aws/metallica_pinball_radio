8320202a279b5fe91cbbefc892757f322500dbbf
rekky1aws <killikomfr@gmail.com>
2024-12-17 20:19:19 +0100

    Method to get hashCode from String
     + @ String.prototype.hashCode() :
       Get a hashcode depending on the string. Used here to generate gauge
       filling percentage.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+String.prototype.hashCode = function ()
+{
+	let hash = 0;
+	let i, chr;
+	if (this.length === 0) {
+		return hash;
+	}
+	for(i = 0; i < this.length; i++) {
+		chr = this.charCodeAt(i);
+		hash = ((hash << 5) - hash) + chr;
+		hash |= 0;
+	}
+	return hash;
+}
+
 // CLASS
 class MusicTrack {
 	constructor (title="", videoCode="", startTime=0) 
